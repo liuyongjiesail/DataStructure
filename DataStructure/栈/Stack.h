ae1cb7027bb4a917b295d614e8bada6cec6a1680
刘永杰 <liuyongjiesail@icloud.com>
2018-08-08 16:01:31 +0800

    Add file directory

---
+++ DataStructure/栈/Stack.h
@@ -0,0 +1,14 @@
+//
+//  Stack.h
+//  DataStructure
+//
+//  Created by 刘永杰 on 2018/8/8.
+//  Copyright © 2018年 刘永杰. All rights reserved.
+//
+
+#ifndef Stack_h
+#define Stack_h
+
+#include <stdio.h>
+
+#endif /* Stack_h */
---
+++ DataStructure/队列/Queue.h
@@ -0,0 +1,14 @@
+//
+//  Queue.h
+//  DataStructure
+//
+//  Created by 刘永杰 on 2018/8/8.
+//  Copyright © 2018年 刘永杰. All rights reserved.
+//
+
+#ifndef Queue_h
+#define Queue_h
+
+#include <stdio.h>
+
+#endif /* Queue_h */
